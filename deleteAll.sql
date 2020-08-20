63f579a80d71e292edbb059d5f592f57580c2365
abrusut <andresbrusutti@gmail.com>
2020-08-20 08:22:09 -0300

    delete files

--- deleteAll.sql
+++
@@ -1,5 +0,0 @@
-delete FROM mprod_subsidio.excel_ingreso;
-delete FROM mprod_subsidio.subsidio_pago_proveedores;
-delete FROM mprod_subsidio.cabecera;
-delete FROM mprod_subsidio.totales;
-delete FROM mprod_subsidio.requisito;
